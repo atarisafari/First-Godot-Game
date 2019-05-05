f542d2a671ffdb4148aa92af77db541c423f7457
Ethan Finlay <efinclarke1993@gmail.com>
2019-05-04 21:54:36 -0400

    Testing features with new level

--- firstGame/Player.gd
+++ firstGame/Player.gd
@@ -17,13 +17,13 @@ func _ready():
 func _physics_process(delta):
 	direction = Vector3(0, 0, 0)
 	if Input.is_action_pressed("ui_left"):
-		direction.z += 1
-	elif Input.is_action_pressed("ui_right"):
-		direction.z -=1
-	elif Input.is_action_pressed("ui_down"):
-		direction.x += 1
-	elif Input.is_action_pressed("ui_up"):
 		direction.x -= 1
+	elif Input.is_action_pressed("ui_right"):
+		direction.x +=1
+	elif Input.is_action_pressed("ui_down"):
+		direction.z += 1
+	elif Input.is_action_pressed("ui_up"):
+		direction.z -= 1
 	direction = direction.normalized()
 	direction = direction * speed * delta
 	
